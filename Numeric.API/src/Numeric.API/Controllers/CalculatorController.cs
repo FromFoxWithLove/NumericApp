647b0c66afc8a7b79d083b871ac9eb0d5f113531
Danil <you@example.com>
2022-06-18 15:10:40 +0300

    empty api project

---
+++ Numeric.API/src/Numeric.API/Controllers/CalculatorController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Numeric.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+    }
+}
